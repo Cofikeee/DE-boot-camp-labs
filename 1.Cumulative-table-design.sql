a6cb44d55d410aeaf6e1121beb46e4bd1a8ed24f
Cofikeee <116801166+Cofikeee@users.noreply.github.com>
2024-11-23 05:39:33 +0300

    Update 1.Cumulative-table-design.sql

--- 1.Cumulative-table-design.sql
+++ 1.Cumulative-table-design.sql
@@ -86,8 +86,6 @@ SELECT
 FROM today t
     FULL OUTER JOIN yesterday y USING (player_name);
 
-SELECT * FROM players;
-
 /*
 WITH unnested as (SELECT player_name,
                          UNNEST(season_stats)::season_stats AS season_stats
@@ -98,3 +96,11 @@ SELECT player_name,
        (season_stats::season_stats).*
 FROM unnested
  */
+
+SELECT
+    player_name,
+        (season_stats[cardinality(season_stats)]::season_stats).pts/
+        CASE WHEN (season_stats[1]::season_stats).pts = 0 THEN 1 ELSE (season_stats[1]::season_stats).pts END AS progress
+FROM players
+WHERE current_season = 2001 AND scoring_class = 'star'
+ORDER BY 2 DESC
