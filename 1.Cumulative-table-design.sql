462d06b2ac3e24c44877479b172249d94184fbae
Cofikeee <116801166+Cofikeee@users.noreply.github.com>
2024-11-24 13:36:57 +0300

    Update 1.Cumulative-table-design.sql

--- 1.Cumulative-table-design.sql
+++ 1.Cumulative-table-design.sql
@@ -1,5 +1,5 @@
-/*
 CREATE TYPE season_stats AS (
+
     season INT,
     gp INT,
     pts REAL,
@@ -9,6 +9,7 @@ CREATE TYPE season_stats AS (
 
 CREATE TYPE scoring_class AS ENUM ('star', 'good', 'average', 'bad');
 
+DROP TABLE IF EXISTS players;
 CREATE TABLE IF NOT EXISTS players (
     player_name TEXT,
     height TEXT,
@@ -21,86 +22,83 @@ CREATE TABLE IF NOT EXISTS players (
     scoring_class scoring_class,
     years_since_last_season INT,
     current_season INT,
+    is_active BOOLEAN,
     PRIMARY KEY(player_name, current_season)
 );
 
 
-*/
 
 INSERT INTO players
-WITH yesterday AS (
+WITH years AS (
     SELECT *
-    FROM players
-    WHERE current_season = (SELECT MAX(current_season) FROM players)
-),
-    today AS (
-        SELECT *
+    FROM generate_series(1996,2022) as season
+    ),
+    p AS (
+        SELECT
+            player_name,
+            min(season) AS first_season
         FROM player_seasons
-        WHERE season = (SELECT MAX(current_season)+1 FROM players)
+        GROUP BY player_name
+    ),
+    players_and_seasons AS (
+        SELECT *
+        FROM p
+        JOIN years y ON p.first_season <= y.season
+
+    ),
+    windowed AS (
+            SELECT pas.player_name,
+                   pas.season,
+                   ARRAY_REMOVE(
+                        ARRAY_AGG(
+                            CASE
+                                WHEN ps.season IS NOT NULL
+                                    THEN ROW(
+                                        ps.season,
+                                        ps.gp,
+                                        ps.pts,
+                                        ps.reb,
+                                        ps.ast
+                                        )::season_stats
+                            END)
+                        OVER (PARTITION BY pas.player_name ORDER BY COALESCE(pas.season, ps.season)),
+                        NULL
+                   ) AS seasons
+        FROM players_and_seasons pas
+        LEFT JOIN player_seasons ps USING(player_name, season)
+        ORDER BY pas.player_name, pas.season
+    ),
+    static AS (
+        SELECT
+            player_name,
+            MAX(height) AS height,
+            MAX(college) AS college,
+            MAX(country) AS country,
+            MAX(draft_year) AS draft_year,
+            MAX(draft_round) AS draft_round,
+            MAX(draft_number) AS draft_number
+        FROM player_seasons
+        GROUP BY player_name
     )
 
 SELECT
-    COALESCE(t.player_name, y.player_name) AS player_name,
-    COALESCE(t.height, y.height) AS height,
-    COALESCE(t.college, y.college) AS college,
-    COALESCE(t.country, y.country) AS country,
-    COALESCE(t.draft_year, y.draft_year) AS draft_year,
-    COALESCE(t.draft_round, y.draft_round) AS draft_year,
-    COALESCE(t.draft_number, y.draft_number) AS draft_number,
+    w.player_name,
+    s.height,
+    s.college,
+    s.country,
+    s.draft_year,
+    s.draft_round,
+    s.draft_number,
+    seasons AS season_stats,
     CASE
-        WHEN y.season_stats IS NULL
-            THEN ARRAY[ROW(
-            t.season,
-            t.gp,
-            t.pts,
-            t.reb,
-            t.ast
-            )::season_stats]
-        WHEN t.season IS NOT NULL
-            THEN y.season_stats || ARRAY[ROW(
-            t.season,
-            t.gp,
-            t.pts,
-            t.reb,
-            t.ast
-            )::season_stats]
-        ELSE
-            y.season_stats
-    END AS season_stats,
-    CASE
-        WHEN t.season IS NOT NULL THEN
-        CASE
-            WHEN  t.pts > 20 THEN 'star'
-            WHEN t.pts > 15 THEN 'good'
-            WHEN t.pts > 10 THEN  'average'
-            ELSE 'bad'
-        END::scoring_class
-        ELSE y.scoring_class
-    END AS scoring_class,
-    CASE
-        WHEN t.season IS NOT NULL THEN 0
-        ELSE y.years_since_last_season + 1
-    END AS years_since_last_season,
-    COALESCE(t.season, y.current_season + 1) as current_season
-
-FROM today t
-    FULL OUTER JOIN yesterday y USING (player_name);
-
-/*
-WITH unnested as (SELECT player_name,
-                         UNNEST(season_stats)::season_stats AS season_stats
-                  FROM players
-                  WHERE current_season = 2001
-                    AND player_name = 'Michael Jordan')
-SELECT player_name,
-       (season_stats::season_stats).*
-FROM unnested
- */
-
-SELECT
-    player_name,
-        (season_stats[cardinality(season_stats)]::season_stats).pts/
-        CASE WHEN (season_stats[1]::season_stats).pts = 0 THEN 1 ELSE (season_stats[1]::season_stats).pts END AS progress
-FROM players
-WHERE current_season = 2001 AND scoring_class = 'star'
-ORDER BY 2 DESC
+        WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 20 THEN 'star'
+        WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 15 THEN 'good'
+        WHEN (seasons[CARDINALITY(seasons)]::season_stats).pts > 10 THEN 'average'
+        ELSE 'bad'
+    END::scoring_class AS scoring_class,
+    w.season - (seasons[CARDINALITY(seasons)]::season_stats).season as years_since_last_active,
+    w.season,
+    (seasons[CARDINALITY(seasons)]::season_stats).season = season AS is_active
+FROM windowed w
+JOIN static s
+    ON w.player_name = s.player_name;
